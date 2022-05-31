a94c097797f43ffba9f96fb4721fcb3fe9e0f778
m2lovric <m2lovric@gmail.com>
2022-05-31 22:28:43 +0200

    Add Login and Register page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Layout } from '../components';
+
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { Layout } from '../../components';
+import './style.scss';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth } from '../../../modules/firebase';
+
+const Register = () => {
+  const [data, setData] = useState({ name: '', email: '', password: '' });
+
+  const handleSubmit = () => {
+    console.log('click');
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+      .then((res) => {
+        const user = res.user;
+        updateProfile(user, { displayName: data.name });
+        setData({ name: '', email: '', password: '' });
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error);
+      });
+  };
+
+  return (
+    <Layout>
+      <section className='register'>
+        <section className='register__form'>
+          <label htmlFor='name' className='input-label'>
+            Name
+          </label>
+          <input
+            className='input-lg'
+            type='text'
+            name='name'
+            placeholder='Name'
+            onChange={(e) => setData({ ...data, name: e.target.value })}
+            value={data.name}
+          />
+
+          <label htmlFor='email' className='input-label'>
+            Email
+          </label>
+          <input
+            className='input-lg'
+            type='text'
+            name='email'
+            placeholder='Email'
+            onChange={(e) => setData({ ...data, email: e.target.value })}
+            value={data.email}
+          />
+
+          <label htmlFor='name' className='input-label'>
+            Password
+          </label>
+          <input
+            className='input-lg'
+            type='password'
+            name='password'
+            placeholder='Password'
+            onChange={(e) => setData({ ...data, password: e.target.value })}
+            value={data.password}
+          />
+          <button className='btn-primary' onClick={() => handleSubmit()}>
+            Register
+          </button>
+        </section>
+      </section>
+    </Layout>
+  );
+};
+
+export default Register;
