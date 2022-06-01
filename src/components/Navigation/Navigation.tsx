6441195431063f00fe78444410cd5a101b50494b
m2lovric <m2lovric@gmail.com>
2022-06-01 23:58:50 +0200

    Link to homepage

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 import hamburger from '../../assets/hamburger.svg';
 import './style.scss';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <nav className='nav'>
       <section className='container'>
-        <img src={logo} alt='logo' className='nav__logo' />
+        <Link to={'/'} className='nav__logo'>
+          <img src={logo} alt='logo' />
+        </Link>
+
         <img src={hamburger} alt='menu' className='nav__menu' />
       </section>
     </nav>
