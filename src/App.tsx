6973cfa08e65153390c84f7d5418280d75f25a90
m2lovric <m2lovric@gmail.com>
2022-06-01 17:56:40 +0200

    Home update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { Layout } from './components';
+import './app.scss';
+import car from './assets/front-left-2.png';
+
 function App() {
   return (
-    <main className='App'>
-      <section>Home</section>
-    </main>
+    <Layout>
+      <section className='home'>
+        <header className='home__header'>
+          <h2 className='home__heading'>View saved configurations</h2>
+          <button className='btn-primary'>Configure a car</button>
+        </header>
+        <img src={car} alt='car' className='home__img' />
+        <p className='home__text'>
+          You haven't configured any cars yet. You may choose to
+          <span> configure some now.</span>
+        </p>
+      </section>
+    </Layout>
   );
 }
 
