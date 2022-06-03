5166f33a237692d36de3b735007b02a8a66211a8
m2lovric <m2lovric@gmail.com>
2022-06-03 14:14:12 +0200

    Remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,14 @@ import { Register, Login, Select } from './pages';
 import './scss/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<App />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/select' element={<Select />} />
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
-  </React.StrictMode>
+  <RecoilRoot>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/select' element={<Select />} />
+      </Routes>
+    </BrowserRouter>
+  </RecoilRoot>
 );
