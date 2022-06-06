35770e5d7db413750edbc2a0a4b592f8db89044d
m2lovric <m2lovric@gmail.com>
2022-06-06 23:58:41 +0200

    Add firebase storage

--- modules/firebase/index.ts
+++ modules/firebase/index.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -15,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth();
+const storage = getStorage(app);
 
-export { db, auth };
+export { db, auth, storage };
--- src/pages/Select/Select.tsx
+++ src/pages/Select/Select.tsx
@@ -1,13 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import { Layout, Car } from '../../components';
 import { collection, query, getDocs, DocumentData } from 'firebase/firestore';
-import { db } from '../../../modules/firebase';
+import { ref, getDownloadURL } from 'firebase/storage';
+import { db, storage } from '../../../modules/firebase';
 import { CarI } from '../../components/Car/Car';
 import './select.scss';
 import { motion } from 'framer-motion';
 
 const Select = () => {
   const [models, setModels] = useState<CarI[]>([]);
+
+  const starsRef = ref(
+    storage,
+    `RS5/Car=RS5, View=Side, Color=Red, Wheel Style=One.png`
+  );
+
+  getDownloadURL(starsRef)
+    .then((url) => {
+      console.log(url);
+    })
+    .catch((error) => {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/object-not-found':
+          // File doesn't exist
+          break;
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+        // ...
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    });
 
   useEffect(() => {
     fetchData();
@@ -31,10 +63,18 @@ const Select = () => {
         <motion.div className='carousel select__carousel'>
           <motion.div
             drag='x'
-            dragConstraints={{ right: 0 }}
+            dragConstraints={{ right: 0, left: -500 }}
             className='inner-carousel select__cars '
           >
-            {models ? models.map((el: any) => <Car data={el} />) : ''}
+            {models
+              ? models.map((el: any) => {
+                  return (
+                    <motion.div key={el.id}>
+                      <Car data={el} />
+                    </motion.div>
+                  );
+                })
+              : ''}
           </motion.div>
         </motion.div>
       </section>
