863e5c71ff098a73bb8696f8a5c092a69ca22987
m2lovric <m2lovric@gmail.com>
2022-06-06 23:58:24 +0200

    Add exterior page

---
+++ src/pages/Configuration/Exterior.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from '../../components';
+
+const Exterior = () => {
+  return (
+    <Layout>
+      <section className='exterior'></section>
+    </Layout>
+  );
+};
+
+export default Exterior;
