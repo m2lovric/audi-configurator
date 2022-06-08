36b783ab4104b9790b130a1237cbea3ed3d7fd20
m2lovric <m2lovric@gmail.com>
2022-06-09 00:23:37 +0200

    Export wheels

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Layout from './Layout';
 import Car from './Car/Car';
 import ConfiguratorNav from './ConfiguratorNav/ConfiguratorNav';
 import Colors from './Colors/Colors';
+import Wheels from './Wheels/Wheels';
 
-export { Layout, Car, ConfiguratorNav, Colors };
+export { Layout, Car, ConfiguratorNav, Colors, Wheels };
