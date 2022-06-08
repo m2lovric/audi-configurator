d8cb60d6215a67c41f712e0ea0c9a77a3eda3e27
m2lovric <m2lovric@gmail.com>
2022-06-08 19:04:40 +0200

    Add colors state

--- modules/state/atoms.ts
+++ modules/state/atoms.ts
@@ -1,7 +1,14 @@
 import { atom } from 'recoil';
 import { User } from 'firebase/auth';
 
+import { modelConfigI } from '../../src/pages/Configuration/Exterior';
+
 export const userStateAtom = atom({
   key: 'user',
   default: false,
+});
+
+export const configModelsAtom = atom({
+  key: 'configModel',
+  default: <modelConfigI>{},
 });
