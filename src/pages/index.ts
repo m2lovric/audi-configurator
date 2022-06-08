3cb334d5dbef501317668537a1b5ce0df13055b5
m2lovric <m2lovric@gmail.com>
2022-06-08 19:05:56 +0200

    Export exterior page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 import Login from './Login/Login';
 import Register from './Register/Register';
 import Select from './Select/Select';
-export { Login, Register, Select };
+import Exterior from './Configuration/Exterior';
+export { Login, Register, Select, Exterior };
