165c85fc1ac8563e4fb6947de9db1947bd7fa43d
m2lovric <m2lovric@gmail.com>
2022-06-12 23:49:59 +0200

    Import Interior page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,5 @@ import Login from './Login/Login';
 import Register from './Register/Register';
 import Select from './Select/Select';
 import Exterior from './Configuration/Exterior';
-export { Login, Register, Select, Exterior };
+import Interior from './Configuration/Interior/Interior';
+export { Login, Register, Select, Exterior, Interior };
