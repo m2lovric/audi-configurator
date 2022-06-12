54b20e5023166e78ff72168ece73849f16f190fe
m2lovric <m2lovric@gmail.com>
2022-06-12 23:47:16 +0200

    Import InteriorColors component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Car from './Car/Car';
 import ConfiguratorNav from './ConfiguratorNav/ConfiguratorNav';
 import Colors from './Colors/Colors';
 import Wheels from './Wheels/Wheels';
+import InteriorColors from './InteriorColors/InteriorColors';
 
-export { Layout, Car, ConfiguratorNav, Colors, Wheels };
+export { Layout, Car, ConfiguratorNav, Colors, Wheels, InteriorColors };
