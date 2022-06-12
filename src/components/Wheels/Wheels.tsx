bf58adc2964cdd43e933e44acd3162289e7b1fe7
m2lovric <m2lovric@gmail.com>
2022-06-12 23:49:36 +0200

    Add cancel funcionality

--- src/components/Wheels/Wheels.tsx
+++ src/components/Wheels/Wheels.tsx
@@ -2,12 +2,26 @@ import { getDownloadURL, ref } from 'firebase/storage';
 import React, { useEffect, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { storage } from '../../../modules/firebase';
-import { configModelsAtom, wheelsAtom } from '../../../modules/state/atoms';
+import cancel from '../../assets/X.png';
+import {
+  configModelsAtom,
+  wheelsAtom,
+  visibleAtom,
+  visibleAtomA,
+} from '../../../modules/state/atoms';
 
 const Wheels = () => {
   const modelConfig = useRecoilValue(configModelsAtom);
   const [wheels, setWheels] =
     useRecoilState<{ name: string; url: string; price: number }[]>(wheelsAtom);
+
+  const [visible, setVisible] = useRecoilState(visibleAtom);
+  const [visibleA, setVisibleA] = useRecoilState(visibleAtomA);
+
+  const handleCancel = () => {
+    setVisible({ ...visible, wheels: false });
+    setVisibleA({ ...visibleA, wheels: false });
+  };
 
   useEffect(() => {
     modelConfig.wheels.map((el) => {
@@ -31,7 +45,14 @@ const Wheels = () => {
   }, []);
 
   return (
-    <section>
+    <section className='colors'>
+      <section className='colors__heading'>
+        <h3>Wheels</h3>
+        <button className='colors__btn' onClick={() => handleCancel()}>
+          <img src={cancel} alt='cancel' />
+        </button>
+      </section>
+
       {wheels
         ? wheels.map((el) => {
             return (
@@ -45,6 +66,12 @@ const Wheels = () => {
             );
           })
         : ''}
+
+      <section className='colors__total'>
+        <p>TOTAL</p>
+        <p>1200000&euro;</p>
+      </section>
+      <button className='btn-primary-lg'>Done</button>
     </section>
   );
 };
