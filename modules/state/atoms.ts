c31002fb6dbcf0bb2c98630e58500108651f302f
m2lovric <m2lovric@gmail.com>
2022-06-12 23:46:42 +0200

    Add new atoms

--- modules/state/atoms.ts
+++ modules/state/atoms.ts
@@ -22,3 +22,18 @@ export const wheelsAtom = atom({
   key: 'wheelsModel',
   default: <{ name: string; url: string; price: number }[]>[],
 });
+
+export const interiorAtom = atom({
+  key: 'interiorModel',
+  default: <{ name: string; url: string; price: number }[]>[],
+});
+
+export const visibleAtom = atom({
+  key: 'visibleModel',
+  default: { colors: false, wheels: false },
+});
+
+export const visibleAtomA = atom({
+  key: 'visibleModelA',
+  default: { colors: false, wheels: false },
+});
