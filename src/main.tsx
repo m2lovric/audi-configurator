643e5982aeafe861a47d7de6ae048f84ef36f4fd
m2lovric <m2lovric@gmail.com>
2022-06-17 12:35:33 +0200

    Add new route

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import { Register, Login, Select, Exterior, Interior } from './pages';
+import { Register, Login, Select, Exterior, Interior, Summary } from './pages';
 import './scss/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -16,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/select' element={<Select />} />
         <Route path='/configure/exterior/:year/:model' element={<Exterior />} />
         <Route path='/configure/interior/:year/:model' element={<Interior />} />
+        <Route path='/configure/summary/:year/:model' element={<Summary />} />
       </Routes>
     </BrowserRouter>
   </RecoilRoot>
