019718a12a0b190b9728d5f15484deb07ca4bd16
m2lovric <m2lovric@gmail.com>
2022-07-05 18:51:29 +0200

    Use selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { Layout } from './components';
 import './app.scss';
 import car from './assets/front-left-2.png';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import {
-  configModelsAtom,
-  userConfiguration,
-  userIdAtom,
-  userStateAtom,
-} from 'modules/state/atoms';
+import { atom, selector, useRecoilState, useRecoilValue } from 'recoil';
+import { configModelsAtom, userConfiguration } from 'modules/state/atoms';
 import { useNavigate, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import {
@@ -21,9 +16,28 @@ import { db } from 'modules/firebase/index';
 import { Model } from 'modules/interfaces/model';
 import dots from './assets/Union.svg';
 
+const userStateAtom = atom({
+  key: 'userState',
+  default: false,
+});
+
+const userIdAtom = atom({
+  key: 'userId',
+  default: '',
+});
+
+const userInfoSelector = selector({
+  key: 'userInfo',
+  get: ({ get }) => {
+    const user = get(userStateAtom);
+    const userId = get(userIdAtom);
+
+    return { user, userId };
+  },
+});
+
 function App() {
-  const user = useRecoilValue(userStateAtom);
-  const userId = useRecoilValue(userIdAtom);
+  const { user, userId } = useRecoilValue(userInfoSelector);
   const [savedConfigs, setSavedConfigs] = useState<Model[]>([]);
   const [selectedValues, setSelectedValues] = useRecoilState(userConfiguration);
   const [modelConfig, setModelConfig] = useRecoilState(configModelsAtom);
