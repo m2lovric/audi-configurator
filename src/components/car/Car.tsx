8072a0d0bb0d133e5a170a356e8ee3c2e109edea
m2lovric <m2lovric@gmail.com>
2022-07-11 22:58:46 +0200

    Add update hook to Car component

--- src/components/car/Car.tsx
+++ src/components/car/Car.tsx
@@ -2,47 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './car.scss';
 import { CarInterface } from 'modules/interfaces/car';
-import { Model } from 'modules/interfaces/model';
-import { useRecoilState } from 'recoil';
-import {
-  colorsAtom,
-  interiorAtom,
-  selectModelAtom,
-  userConfigurationAtom,
-  wheelsAtom,
-} from 'modules/state/index';
+import useUpdate from './useUpdate';
 
 const Car = ({ data }: CarInterface) => {
-  const [selectedValues, setSelectedValues] = useRecoilState(
-    userConfigurationAtom
-  );
-  const [colorsState, setColorState] = useRecoilState(colorsAtom);
-  const [wheelsState, setWheelsState] = useRecoilState(wheelsAtom);
-  const [interiorState, setInteriorState] = useRecoilState(interiorAtom);
-  const [selectModels, setSelectModels] =
-    useRecoilState<Model[]>(selectModelAtom);
-
-  const handleUpdate = () => {
-    const car = selectModels.filter(
-      (el) => el.model == data.model.split(' ')[1]
-    )[0];
-
-    setSelectedValues({
-      ...selectedValues,
-      fullName: data.model,
-      model: data.model.split(' ')[1],
-      accessories: {
-        color: { ...car.default.color },
-        interior: { ...car.default.interior },
-        wheel: { ...car.default.wheels },
-      },
-      price: car.price,
-    });
-
-    setColorState([]);
-    setWheelsState([]);
-    setInteriorState([]);
-  };
+  const [handleUpdate] = useUpdate();
 
   return (
     <div>
@@ -53,7 +16,7 @@ const Car = ({ data }: CarInterface) => {
         <Link
           to={`/configure/exterior/${data.production_year}/${data.model}`}
           className='btn-primary'
-          onClick={handleUpdate}
+          onClick={() => handleUpdate(data)}
         >
           Configure Now
         </Link>
---
+++ src/components/car/useUpdate.ts
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Model } from 'modules/interfaces/model';
+import {
+  selectModelAtom,
+  userConfigurationAtom,
+  colorsAtom,
+  wheelsAtom,
+  interiorAtom,
+} from 'modules/state/';
+import { useRecoilState } from 'recoil';
+import { CarInterface } from '@/../modules/interfaces/car';
+
+const useUpdate = () => {
+  const [selectModels, setSelectModels] =
+    useRecoilState<Model[]>(selectModelAtom);
+  const [selectedValues, setSelectedValues] = useRecoilState(
+    userConfigurationAtom
+  );
+  const [colorsState, setColorState] = useRecoilState(colorsAtom);
+  const [wheelsState, setWheelsState] = useRecoilState(wheelsAtom);
+  const [interiorState, setInteriorState] = useRecoilState(interiorAtom);
+
+  const handleUpdate = (data: any) => {
+    console.log(data.model);
+    const car = selectModels.filter(
+      (el) => el.model == data.model.split(' ')[1]
+    )[0];
+
+    setSelectedValues({
+      ...selectedValues,
+      fullName: data.model,
+      model: data.model.split(' ')[1],
+      accessories: {
+        color: { ...car.default.color },
+        interior: { ...car.default.interior },
+        wheel: { ...car.default.wheels },
+      },
+      price: car.price,
+    });
+    setColorState([]);
+    setWheelsState([]);
+    setInteriorState([]);
+  };
+  return [handleUpdate];
+};
+
+export default useUpdate;
